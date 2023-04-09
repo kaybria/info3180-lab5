c85e75ac5199f0f0e12621083d8fac6d842085b6
Kali Soutar <kalisoutar@gmail.com>
2023-04-09 16:52:54 -0500

    added css to movies

--- app/views.py
+++ app/views.py
@@ -21,47 +21,43 @@ from flask_wtf.csrf import generate_csrf
 def index():
     return jsonify(message="This is the beginning of our API")
 
-@app.route('/api/v1/movies', methods=['POST','GET'])
-def movies():
-    print("From")
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():    
     movieform = MovieForm()
-    if request.method == 'POST':
-        
-        print('hello1')
-        if movieform.validate_on_submit():
-            title= movieform.title.data
-            description = movieform.description.data
-            poster = movieform.poster.data
-            created_at = datetime.utcnow()
-            pname=secure_filename(poster.filename)
-            poster.save(os.path.join(app.config['UPLOAD_FOLDER'],pname))
+    if movieform.validate_on_submit():
+        title= movieform.title.data
+        description = movieform.description.data
+        poster = movieform.poster.data
+        created_at = datetime.utcnow()
+        pname=secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'],pname))
 
-            newmovie = Movie(title,description,pname,created_at)
-            db.session.add(newmovie)
-            db.session.commit()
-            
-            movieresult = {
-                "message": 'Movie Successfully added',
-                "title": title,
-                "poster": pname,
-                "description": description}
-            return jsonify(data = movieresult)
-        return jsonify(errors =form_errors(movieform))
-    else:
-         movies = Movie.query.all()
-         allmovies = []
-         for m in movies:
-            allmovies.append({
+        newmovie = Movie(title,description,pname,created_at)
+        db.session.add(newmovie)
+        db.session.commit()
+        
+        movieresult = {
+            "message": 'Movie Successfully added',
+            "title": title,
+            "poster": pname,
+            "description": description}
+        return jsonify(data = movieresult)
+    return jsonify(errors =form_errors(movieform))
+
+    
+
+@app.route('/api/v1/movies', methods=['GET'])
+def getmovies():
+    movies = Movie.query.all()
+    allmovies = []
+    for m in movies:
+        allmovies.append({
             "id":m.id,
             "title" :m.title,
             "description":m.description,
             "poster": url_for('getimages',filename = m.poster)
         })
     return jsonify(movies=allmovies)
-
-
- 
-   
 
 @app.route('/api/v1/csrf-token', methods=['GET'])
 def get_csrf():
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -16,16 +16,56 @@ let movies = ref([]);
    
 
 <template>
+  <h2>Movies</h2>
   <div class="moviecontainer">
-    <h2>Movies</h2>
-    <div class = "card" v-for="m in movies">
-     <img :src="m.poster" alt = "poster" />
-    <h1>{{m.title}}</h1>
-    <p>{{m.description}}</p>
+    
+    <div class = "movie" v-for="m in movies">
+      <div class = "poster">
+        <img :src="m.poster" alt = "poster" />
+      </div>
+
+      <div class = "info">
+        <h1>{{m.title}}</h1>
+        <p>{{m.description}}</p>
+      </div>
+
     </div>
   </div>
 </template>
 
 <style>
+  .moviecontainer{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:space-evenly;
+    
+}
+h1,p{
+  padding:10px;
+}
+h2{
+  padding:20px;
+}
+.poster{
+  flex: 0 0 40%;
+}
+
+img{
+    width: 100%;
+    height: 100%;
+     
+}
+.movie{
+  display:flex;
+  flex-direction:row;
+  width:30%;
+  height:10%;
+  border: 2px;
+  box-shadow: 0 0 6px;
+  border-radius: 8px;
+  padding: 20px;
+  justify-content: center;
+}
+ 
 </style>
 
