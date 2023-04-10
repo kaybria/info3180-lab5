c50366ded970da370ee5b62b6b867fee7b72820e
Kali Soutar <kalisoutar@gmail.com>
2023-04-10 09:26:06 -0500

    added flash message to form

--- src/components/MovieForm.vue
+++ src/components/MovieForm.vue
@@ -1,9 +1,13 @@
 <script setup>
   import { ref, onMounted } from "vue";
   let csrf_token = ref("");
+  let FlashMessage = ref("");
+  let danger = ref(false);
+  let success = ref("");
   onMounted(() => {
     getCsrfToken();
   });
+
   function getCsrfToken() {
     fetch('/api/v1/csrf-token')
       .then((response) => response.json())
@@ -12,9 +16,11 @@
         csrf_token.value = data.csrf_token;
       })
     }
+
   function saveMovie() {
   let movieForm =document.getElementById('movieForm');
   let form_data = new FormData(movieForm);
+
   fetch("/api/v1/movies", {
     method: 'POST',
     body: form_data,
@@ -26,18 +32,40 @@
     return response.json();
     })
     .then(function (data) {
-    // display a success message
-    console.log(data);
+      if(data.errors){
+        FlashMessage.value = data.errors;
+        danger.value = true;
+        clearFormData();
+         
+      }
+      if (data.data){
+        FlashMessage.value = "Movie Added Successfully!";
+         success.value = true;
+         danger.value = false;
+         clearFormData();
+         console.log(data);
+         
+      }
+    
     })
     .catch(function (error) {
-    console.log(error);
+      console.log(error);
     });
+    }
+    function clearFormData(){
+       var data = document.querySelectorAll('input');
+       data.forEach(input => input.value='');
     }
   
 </script>
 
 <template>
   <div class = "formbox">
+    <div v-if="success" class = "alert alert-success">{{FlashMessage}}</div>
+    <div v-if ="danger" class = "alert alert-danger"> 
+      <li v-for= "d in FlashMessage">{{d}}</li>
+    </div>
+    
     <form id = "movieForm" @submit.prevent="saveMovie">
 
     <div class="form-group mb-3">
